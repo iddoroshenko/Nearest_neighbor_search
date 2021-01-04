3b4e077b3198ddc2c8fbfbc20fa935a8214b075a
Ilya Doroshenko <ilya.dor201232@gmail.com>
2021-01-04 19:13:29 +0300

    add knn-search method based on the tabu search

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <chrono>
 #include <thread>
+#include <random>
 #include "tqdm.h"
 
 uint32_t maxAge = 1;
@@ -30,7 +31,7 @@ long double calculateDistance (const Point& point1, const Point& point2) {
     uint64_t distance = 0;
     size_t size = point1.size();
     for(int i = 0; i < size; ++i) {
-        distance += uint64_t(point1[i] - point2[i]) * uint64_t(point1[i] - point2[i]);
+        distance += int64_t(point1[i] - point2[i]) * int64_t(point1[i] - point2[i]);
     }
     return sqrt(distance);
 }
@@ -46,7 +47,7 @@ adjacency_list constructGraph_Naive(const Points& points) {
         for(int j = 0; j < points.size(); ++j) {
             if(i == j) continue;
             nearestK.insert({calculateDistance(points[i], points[j]), j});
-            if(nearestK.size() == 6) {
+            if(nearestK.size() == K + 1) {
                 nearestK.erase(prev(nearestK.end()));
             }
         }
@@ -73,9 +74,9 @@ int addNewPoint(const Point& newPoint, const Points& points, const adjacency_lis
                 goodEdges.push_back(edge);
             }
         }
-        std::sort(goodEdges.begin(), goodEdges.end(), [](const Edge& a, const Edge& b) { return a.age > b.age; });
+        std::sort(goodEdges.begin(), goodEdges.end(), [](const Edge& a, const Edge& b) { return a.age < b.age; });
         for(const auto& edge : goodEdges) {
-            long double dist = calculateDistance(points[indexCurPoint], points[edge.dest]);
+            long double dist = calculateDistance(newPoint, points[edge.dest]);
             if(dist < curDistance) {
                 indexCurPoint = edge.dest;
                 curDistance = dist;
@@ -91,7 +92,48 @@ int addNewPoint(const Point& newPoint, const Points& points, const adjacency_lis
     return indexCurPoint;
 }
 
+std::vector<int> findKNN(const Point& newPoint, const Points& points, const adjacency_list& v) {
+    int indexStartPoint = rand() % points.size();
+    long double dist = calculateDistance(points[indexStartPoint], newPoint);
+    std::set<std::pair<long double, int>> candidates;
+    candidates.insert({dist, indexStartPoint});
+
+    std::vector<bool> visitedPoints(points.size(), false);
+    visitedPoints[indexStartPoint] = true;
+
+    std::set<std::pair<long double, int>> topKNearestPoints;
+    topKNearestPoints.insert({dist, indexStartPoint});
+
+    while(!candidates.empty()) {
+        auto currentPoint = *candidates.begin();
+        candidates.erase(candidates.begin());
+
+        topKNearestPoints.insert(currentPoint);
+        if(topKNearestPoints.size() == K + 1) {
+            if (prev(topKNearestPoints.end())->second == currentPoint.second) {
+                topKNearestPoints.erase(prev(topKNearestPoints.end()));
+                break;
+            }
+            topKNearestPoints.erase(prev(topKNearestPoints.end()));
+        }
+        for(const Edge& edge : v[currentPoint.second]) {
+            if(!visitedPoints[edge.dest]) {
+                visitedPoints[edge.dest] = true;
+                dist = calculateDistance(points[edge.dest], newPoint);
+                candidates.insert({dist, edge.dest});
+            }
+        }
+    }
+    std::vector<int> result;
+    result.reserve(K);
+    for(auto & x : topKNearestPoints) {
+        result.push_back(x.second);
+    }
+    return result;
+}
+
 int main() {
+    srand(time(NULL));
     auto inputFilePath = "../sample.txt";
     std::ifstream file(inputFilePath);
     if (file.is_open()) {
@@ -106,11 +148,19 @@ int main() {
         }
         file.close();
         std::cout << "The file has been read" << '\n';
-
+        /*
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::shuffle(v.begin(), v.end(), g);
+        */
         std::cout << "\nConstructing data stucture... " << inputFilePath << '\n';
         auto graph = constructGraph_Naive(v);
         std::cout << "\nDone!\n";
-        std::cout << addNewPoint(Point(1000, 395), v, graph) << '\n';
+        std::cout << addNewPoint(Point({1000, 395}), v, graph) + 1<< '\n';
+
+        auto knn = findKNN(Point({1000, 395}), v, graph);
+        for(auto x : knn)
+            std::cout << x + 1<< " " ;
 
         return 0;
     } else {
